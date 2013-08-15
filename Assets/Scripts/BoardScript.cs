66070d385d35a638b5f98033bc119022a09585f5
vialta <virgil.tanase.me@gmail.com>
2013-08-15 11:10:12 +0200

    Commit 11:00 AM
    Starting the Matching Algorithm

--- Assets/Scripts/BoardScript.cs
+++ Assets/Scripts/BoardScript.cs
@@ -5,27 +5,39 @@ public class BoardScript : MonoBehaviour {
 
 	public GameObject[,] Bricks;
 	public GameObject brickPrefab;
-	
-	// Use this for initialization
+	public Vector3 StartVector= new Vector3(-4.5F,-7F,10F);
+
 	void Start () {
 		Bricks = new GameObject[10,10];
 		
-		for (int it1=0;it1<10;it1++)
+		for (int it1=0;it1<10;it1++){
 			for(int it2=0;it2<10;it2++){
 				Bricks[it1,it2]=Instantiate (brickPrefab,new Vector3(-4.5F+it2,-7F+it1,10F),Quaternion.identity) as GameObject;
 			}
-	
+		}
 	}
 	
-	// Update is called once per frame
 	void Update () {
 	
-	}
-	
-	void checkForNull(){
-	}
-	
-	void OnMouseOver(){
+		if(Input.GetMouseButtonDown(0)){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray,out hit)){
+				Debug.Log (hit.transform.position-StartVector);
+				if(hit.transform.gameObject.name=="Brick(Clone)"){
+					Vector3 hitObject=hit.transform.position-StartVector;
+					MatchAlgorithm(Mathf.FloorToInt(hitObject.x),Mathf.FloorToInt(hitObject.y));
+				}
+			}
+			
+		}
 		
 	}
+	
+	void CheckForNull(){
+	}
+	
+	void MatchAlgorithm(int brickPositionX,int brickPositionY){
+	}
+	
 }
--- Assets/Scripts/BrickClass.cs
+++ Assets/Scripts/BrickClass.cs
@@ -14,7 +14,7 @@ public class BrickClass : MonoBehaviour {
 	}
 	
 	void Update () {
-	
+		
 	}
 	
 }
