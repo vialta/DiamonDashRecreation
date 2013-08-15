ea7403545d7f5c585ddbe40b3012afd2a2d0256c
vialta <virgil.tanase.me@gmail.com>
2013-08-15 10:22:39 +0200

    Set up the Project
    Project Start

---
+++ Assets/Scripts/BoardScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardScript : MonoBehaviour {
+
+	public GameObject[,] Bricks;
+	public GameObject brickPrefab;
+	
+	// Use this for initialization
+	void Start () {
+		Bricks = new GameObject[10,10];
+		
+		for (int it1=0;it1<10;it1++)
+			for(int it2=0;it2<10;it2++){
+				Bricks[it1,it2]=Instantiate (brickPrefab,new Vector3(-4.5F+it2,-7F+it1,10F),Quaternion.identity) as GameObject;
+			}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void checkForNull(){
+	}
+	
+	void OnMouseOver(){
+		
+	}
+}
---
+++ Assets/Scripts/BrickClass.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickClass : MonoBehaviour {
+	
+	public int brickValue;
+	public Material[] brickMaterials;
+	public bool toBeDestroyed;
+	public bool hasBeenChecked;
+		
+	void Start () {
+		brickValue = Random.Range(0,4);
+		this.renderer.material=brickMaterials[brickValue];
+	}
+	
+	void Update () {
+	
+	}
+	
+}
