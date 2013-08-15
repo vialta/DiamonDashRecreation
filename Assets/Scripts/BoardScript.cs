c99398eb932d476b1cb32a1af86412038405a443
vialta <virgil.tanase.me@gmail.com>
2013-08-15 15:11:09 +0200

    Commit 15:00 AM 15th Aug 2013
    Basic Gameplay is in place. Click to destroy the bricks, reinstantiating
    bricks and so on. Not pleased with the way it's going. Considering
    trying out with GUI textures instead.
    
    Consider this a rough prototype

--- Assets/Scripts/BoardScript.cs
+++ Assets/Scripts/BoardScript.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class BoardScript : MonoBehaviour {
-
+	
 	public GameObject[,] bricks;
 	public BrickClass[,] brickClass;
 	public GameObject brickPrefab;
@@ -35,14 +35,22 @@ public class BoardScript : MonoBehaviour {
 					if(matchingBrickCounter>=3){
 						for (int it1=0;it1<10;it1++){
 							for(int it2=0;it2<10;it2++){
-								if(brickClass[it1,it2].toBeDestroyed==true){
-									Destroy (bricks[it1,it2]);
-									bricks[it1,it2]=null;
-									Destroy (brickClass[it1,it2]);
-									brickClass[it1,it2]=null;
+								if(brickClass[it1,it2]!=null){						
+									if(brickClass[it1,it2].toBeDestroyed==true){
+										Destroy (bricks[it1,it2]);
+										bricks[it1,it2]=null;
+										Destroy (brickClass[it1,it2]);
+										brickClass[it1,it2]=null;
+									}
 								}
 							}
 						}
+						
+						for(int it1=0;it1<10;it1++){
+							MoveBlocks(it1);
+						}
+						Reinstantiate();
+						PrintBlocks();
 						//Move 
 						//Reinstantiate
 					}
@@ -73,6 +81,26 @@ public class BoardScript : MonoBehaviour {
 		return false;
 	}
 	
+	void MoveBlocks(int brickPositionX){
+		GameObject auxGameObject;
+		BrickClass auxBrickClass;
+		for(int it1=0;it1<9;it1++){
+			if(bricks[brickPositionX,it1]==null){
+				for(int it2=it1;it2<10;it2++){
+					if(bricks[brickPositionX,it2]!=null){
+						auxGameObject=bricks[brickPositionX,it1];
+						auxBrickClass=brickClass[brickPositionX,it1];
+						bricks[brickPositionX,it1]=bricks[brickPositionX,it2];
+						brickClass[brickPositionX,it1]=brickClass[brickPositionX,it2];
+						bricks[brickPositionX,it2]=auxGameObject;
+						brickClass[brickPositionX,it2]=auxBrickClass;
+						it2=10;
+					}
+				}
+			}
+		}
+	}
+	
 	void MatchAlgorithm(int brickPositionX,int brickPositionY){
 		Debug.Log (brickPositionX+" "+brickPositionY);
 		brickClass[brickPositionX,brickPositionY].hasBeenChecked=true;
@@ -99,6 +127,34 @@ public class BoardScript : MonoBehaviour {
 				MatchAlgorithm(brickPositionX,brickPositionY+1);
 			}
 		}		
+	}
+	
+	void PrintBlocks(){
+		string printOut;
+		for(int it1=0;it1<10;it1++){
+			printOut="";
+			for(int it2=0;it2<10;it2++){
+				if(brickClass[it1,it2]==null){
+					printOut+="- ";
+				}
+				else{ printOut+=brickClass[it1,it2].brickValue.ToString()+" ";
+				}
+			}
+			Debug.Log(printOut);
+		}
+	}
+	
+	void Reinstantiate(){
+		while(CheckForNull()){
+			for(int it1=0;it1<10;it1++){
+				for(int it2=0;it2<10;it2++){
+					if(bricks[it1,it2]==null){
+						bricks[it1,it2]=Instantiate (brickPrefab,new Vector3(-4.5F+it1,-5F+it2,10F),Quaternion.identity) as GameObject;
+						brickClass[it1,it2]=bricks[it1,it2].GetComponent<BrickClass>();
+					}
+				}
+			}
+		}
 	}
 	
 }
