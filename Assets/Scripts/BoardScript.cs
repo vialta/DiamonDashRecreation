b5e093b3f20c40daf45a3c739c421b5ff3f0aa1e
vialta <virgil.tanase.me@gmail.com>
2013-08-15 12:07:54 +0200

    Commit 12:00AM
    Mathing Complete. Now moving empty blocks and instantiating new ones

--- Assets/Scripts/BoardScript.cs
+++ Assets/Scripts/BoardScript.cs
@@ -3,16 +3,19 @@ using System.Collections;
 
 public class BoardScript : MonoBehaviour {
 
-	public GameObject[,] Bricks;
+	public GameObject[,] bricks;
+	public BrickClass[,] brickClass;
 	public GameObject brickPrefab;
 	public Vector3 StartVector= new Vector3(-4.5F,-7F,10F);
+	public int matchingBrickCounter;
 
 	void Start () {
-		Bricks = new GameObject[10,10];
-		
+		bricks = new GameObject[10,10];
+		brickClass = new BrickClass[10,10];
 		for (int it1=0;it1<10;it1++){
 			for(int it2=0;it2<10;it2++){
-				Bricks[it1,it2]=Instantiate (brickPrefab,new Vector3(-4.5F+it2,-7F+it1,10F),Quaternion.identity) as GameObject;
+				bricks[it1,it2]=Instantiate (brickPrefab,new Vector3(-4.5F+it1,-7F+it2,10F),Quaternion.identity) as GameObject;
+				brickClass[it1,it2]=bricks[it1,it2].GetComponent<BrickClass>();
 			}
 		}
 	}
@@ -26,18 +29,76 @@ public class BoardScript : MonoBehaviour {
 				Debug.Log (hit.transform.position-StartVector);
 				if(hit.transform.gameObject.name=="Brick(Clone)"){
 					Vector3 hitObject=hit.transform.position-StartVector;
-					MatchAlgorithm(Mathf.FloorToInt(hitObject.x),Mathf.FloorToInt(hitObject.y));
+					Debug.Log (hitObject);
+					MatchAlgorithm(Mathf.FloorToInt(hitObject.x),Mathf.FloorToInt(hitObject.y)+1);
+					Debug.Log (matchingBrickCounter);
+					if(matchingBrickCounter>=3){
+						for (int it1=0;it1<10;it1++){
+							for(int it2=0;it2<10;it2++){
+								if(brickClass[it1,it2].toBeDestroyed==true){
+									Destroy (bricks[it1,it2]);
+									bricks[it1,it2]=null;
+									Destroy (brickClass[it1,it2]);
+									brickClass[it1,it2]=null;
+								}
+							}
+						}
+						//Move 
+						//Reinstantiate
+					}
+					else{
+						for (int it1=0;it1<10;it1++){
+							for(int it2=0;it2<10;it2++){
+								brickClass[it1,it2].toBeDestroyed=false;
+								brickClass[it1,it2].hasBeenChecked=false;
+							}
+						}
+					}
+					matchingBrickCounter=0;
 				}
-			}
-			
+			}		
 		}
+		
 		
 	}
 	
-	void CheckForNull(){
+	bool CheckForNull(){
+		for (int it1=0;it1<10;it1++){
+			for(int it2=0;it2<10;it2++){
+				if(bricks[it1,it2]==null){
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 	
 	void MatchAlgorithm(int brickPositionX,int brickPositionY){
+		Debug.Log (brickPositionX+" "+brickPositionY);
+		brickClass[brickPositionX,brickPositionY].hasBeenChecked=true;
+		brickClass[brickPositionX,brickPositionY].toBeDestroyed=true;
+		matchingBrickCounter+=1;	
+		if(brickPositionX>0){
+			if(brickClass[brickPositionX-1,brickPositionY].hasBeenChecked==false && brickClass[brickPositionX-1,brickPositionY].brickValue==brickClass[brickPositionX,brickPositionY].brickValue){
+				MatchAlgorithm(brickPositionX-1,brickPositionY);
+			}
+			
+		}
+		if(brickPositionX<9){
+			if(brickClass[brickPositionX+1,brickPositionY].hasBeenChecked==false && brickClass[brickPositionX+1,brickPositionY].brickValue==brickClass[brickPositionX,brickPositionY].brickValue){
+				MatchAlgorithm(brickPositionX+1,brickPositionY);
+			}
+		}
+		if(brickPositionY>0){
+			if(brickClass[brickPositionX,brickPositionY-1].hasBeenChecked==false && brickClass[brickPositionX,brickPositionY-1].brickValue==brickClass[brickPositionX,brickPositionY].brickValue){
+				MatchAlgorithm(brickPositionX,brickPositionY-1);
+			}
+		}
+		if(brickPositionY<9){
+			if(brickClass[brickPositionX,brickPositionY+1].hasBeenChecked==false && brickClass[brickPositionX,brickPositionY+1].brickValue==brickClass[brickPositionX,brickPositionY].brickValue){
+				MatchAlgorithm(brickPositionX,brickPositionY+1);
+			}
+		}		
 	}
 	
 }
